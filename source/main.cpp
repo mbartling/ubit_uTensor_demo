dbd720ab88d7ea5d9e2f50893f527cd136dfd10d
Michael Bartling <michael.bartling@arm.com>
2019-07-12 15:31:13 -0500

    Update model to not have cstrings

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include "ubit-utensor-demo/mnist_for_mc.hpp"  //gernerated model file
+#include "ubit-utensor-demo/mnist_for_mc_weight.hpp"  //gernerated model file
 #include "ubit-utensor-demo/input_data.h"  //contains the first sample taken from the MNIST test set
 
 #include "uTensor/core/context.hpp"
@@ -16,7 +17,7 @@ int main(void) {
   Tensor* input_x = new WrappedRamTensor<float>({1, 784}, (float*) input_data);
 
   get_mnist_for_mc_ctx(ctx, input_x);  // pass the tensor to the context
-  S_TENSOR pred_tensor = ctx.get("y_pred:0");  // getting a reference to the output tensor
+  S_TENSOR pred_tensor = ctx.get(sref_mul_6_0);  // getting a reference to the output tensor
   ctx.eval(); //trigger the inference
 
   int pred_label = *(pred_tensor->read<int>(0, 0));  //getting the result back
